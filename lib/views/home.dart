1c12820d0b8bc8d224aae4359fedc61c034f8cb3
renmurakami <autoeyy@gmail.com>
2022-06-21 12:26:27 +0900

    key

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 class HomeView extends HookWidget {
+  const HomeView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
