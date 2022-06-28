1845f3f1fe166bd3cb59d9097bd8de03604bf4b4
renmurakami <autoeyy@gmail.com>
2022-06-28 11:22:04 +0900

    bottom nav bar fixed

--- lib/constants/route.dart
+++ lib/constants/route.dart
@@ -3,10 +3,41 @@ import 'package:cw_mobile_app/views/home.dart';
 import 'package:cw_mobile_app/views/profile.dart';
 import 'package:cw_mobile_app/views/search.dart';
 import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 
-final Routes = <Widget>[
-  HomeView(),
-  SearchView(),
-  FavoriteView(),
-  ProfileView(),
-];
+List<Widget> routeScreens() {
+  return [
+    HomeView(),
+    SearchView(),
+    FavoriteView(),
+    ProfileView(),
+  ];
+}
+
+List<PersistentBottomNavBarItem> navBarItems() {
+  return [
+    PersistentBottomNavBarItem(
+      icon: Icon(Icons.home),
+      activeColorPrimary: Colors.amber.shade700,
+      inactiveColorPrimary: Colors.black54,
+    ),
+    PersistentBottomNavBarItem(
+      icon: Icon(Icons.search),
+      activeColorPrimary: Colors.amber.shade700,
+      inactiveColorPrimary: Colors.black54,
+    ),
+    PersistentBottomNavBarItem(
+      icon: Icon(Icons.favorite),
+      activeColorPrimary: Colors.amber.shade700,
+      inactiveColorPrimary: Colors.black54,
+    ),
+    PersistentBottomNavBarItem(
+      icon: Icon(Icons.manage_accounts),
+      activeColorPrimary: Colors.amber.shade700,
+      inactiveColorPrimary: Colors.black54,
+    ),
+  ];
+}
+
+PersistentTabController navbarController =
+    PersistentTabController(initialIndex: 0);
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:cw_mobile_app/constants/color.dart';
 import 'package:cw_mobile_app/constants/route.dart';
 import 'package:cw_mobile_app/views/home.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,54 +17,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: CustomColor.primaryColor,
-        // primaryColor: Colors.white,
-      ),
-      home: MainApp(),
-    );
-  }
-}
-
-class MainApp extends HookWidget {
-  const MainApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final selectIndex = useState(0);
-    return Scaffold(
-      body: Routes[selectIndex.value],
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.shifting,
-        fixedColor: Colors.amber.shade700,
-        unselectedItemColor: Colors.black54,
-        iconSize: 28,
-        selectedLabelStyle: const TextStyle(fontSize: 12),
-        unselectedFontSize: 10,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "ホーム",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: "探す",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: "お気に入り",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.manage_accounts),
-            label: "プロフィール",
-          ),
-        ],
-        currentIndex: selectIndex.value,
-        onTap: (index) {
-          selectIndex.value = index;
-        },
+      home: PersistentTabView(
+        context,
+        controller: navbarController,
+        screens: routeScreens(),
+        items: navBarItems(),
+        navBarStyle: NavBarStyle.style12,
       ),
     );
   }
