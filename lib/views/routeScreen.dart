44a3fd24f651f3995e2f952dec1b9b1f8182fc4e
renmurakami <autoeyy@gmail.com>
2022-07-21 15:26:51 +0900

    unused

--- lib/views/routeScreen.dart
+++ lib/views/routeScreen.dart
@@ -1,6 +1,5 @@
 import 'package:cw_mobile_app/constants/route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
